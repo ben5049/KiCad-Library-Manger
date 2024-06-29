fdf8e8dccd463ce70a7cb6ab3f51f75fb5e44693
Ben Smith <93447202+ben5049@users.noreply.github.com>
2024-06-29 03:45:01 +0100

    Relinking symbols and footprints basics done

--- source/import_lib.py
+++ source/import_lib.py
@@ -4,10 +4,9 @@ from typing import Optional, Any
 
 from constants import *
 from utils import user_select_from_list
+from parser import Parser
 
 LOGGER = logging.getLogger(__name__)
-
-
 
 
 class ImportLib:
@@ -15,64 +14,72 @@ class ImportLib:
     def __init__(self, input_library_path: Optional[str] = "", library_source: Optional[str] = SOURCE_DEFAULT) -> None:
 
         # Initialise empty attributes
-        self._library_path = None # e.g. "Downloads/LIB_M24256X-FCU6T_VF.zip"
-        self._library = None # e.g. "LIB_M24256X-FCU6T_VF.zip"
-        self._library_extension = None # e.g. "zip"
-        self._library_name = None # e.g. "LIB_M24256X-FCU6T_VF"
-        self._source = None
-        self._imported_symbol = None
+        self._input_library_path      = None # e.g. "../zipfiles/LIB_M24256X-FCU6T_VF.zip"
+        self._input_library_extension = None # e.g. "zip"
+        self._input_source            = None # e.g. "cse"
+
+        self._imported_symbol    = None
         self._imported_footprint = None
-        self._imported_model = None
+        self._imported_model     = None
+
+        self._output_symbol_name    = None
+        self._output_footprint_name = None
+        self._output_model_name     = None
 
         # Apply input values to attributes
-        self.set_input_lib(input_library_path)
+        self.set_input_library(input_library_path)
         self.set_source(library_source)
 
-    def set_input_lib(self, input_library_path: str) -> None:
-        self._library_path = input_library_path
-        self._library = input_library_path.split("/")[-1].split("\\")[-1]
-        self._library_extension = self._library.split(".")[-1] # TODO: Redo with handling for directories
-        self._library_name = self._library.split(".")[0]
+    def set_input_library(self, input_library_path: str) -> None:
+        self._input_library_path = input_library_path
 
+        if self._input_library_path.count("."):
+            self._input_library_extension = self._input_library_path.split(".")[-1] # TODO: Redo with handling for directories
+        else:
+            self._input_library_extension = None
+        
     def set_source(self, library_source: str) -> None:
-        self._source = library_source
+        """
+        Source of the library
+        """
+        self._input_source = library_source
 
     def __import_zipped(self) -> None:
         LOGGER.info(f"Function 'import_cse_zipped' is WIP")
 
-        symbols_found = []
+        symbols_found    = []
         footprints_found = []
-        models_found = []
+        models_found     = []
 
-        symbol_file_name = None
+        symbol_file_name    = None
         footprint_file_name = None
-        model_file_name = None
+        model_file_name     = None
 
-        with ZipFile(self._library_path, "r") as zipfile:
+        with ZipFile(self._input_library_path, "r") as zipfile:
 
             for name in zipfile.namelist():
                 extension = name.split(".")[-1] # TODO: Make sure split is successful
 
-                if extension == SYMBOL_EXTENSION:
+                if extension in SYMBOL_EXTENSIONS:
                     symbols_found.append(name)
-                elif extension == FOOTPRINT_EXTENSION:
+                elif extension in FOOTPRINT_EXTENSIONS:
                     footprints_found.append(name)
-                elif extension == MODEL_EXTENSION:
+                elif extension in MODEL_EXTENSIONS:
                     models_found.append(name)
-            
+
             # Get symbol
             if symbols_found:
                 symbol_file_name = user_select_from_list(symbols_found, "symbol file")
                 LOGGER.info(f"Importing '{symbol_file_name}'")
                 try:
                     with zipfile.open(symbol_file_name, "r") as symbol_file:
-                        self._imported_symbol = symbol_file.read()
+                        self._imported_symbol = symbol_file.read().decode("utf-8")
                 except Exception as e:
                     # TODO: Change the line above to an approprate exception and handle properly
                     LOGGER.error(e)
                     exit(1)
             else:
-                LOGGER.warning(f"No symbols found in library {self._library}")
+                LOGGER.warning(f"No symbols found in library {self._library_path}")
 
             # Get footprint
             if footprints_found:
@@ -80,13 +87,13 @@ class ImportLib:
                 LOGGER.info(f"Importing '{footprint_file_name}'")
                 try:
                     with zipfile.open(footprint_file_name, "r") as footprint_file:
-                        self._imported_footprint = footprint_file.read()
+                        self._imported_footprint = footprint_file.read().decode("utf-8")
                 except Exception as e:
                     # TODO: Change the line above to an approprate exception and handle properly
                     LOGGER.error(e)
                     exit(1)
             else:
-                LOGGER.warning(f"No footprints found in library {self._library}")
+                LOGGER.warning(f"No footprints found in library {self._library_path}")
 
             # Get model
             if models_found:
@@ -94,13 +101,13 @@ class ImportLib:
                 LOGGER.info(f"Importing '{model_file_name}'")
                 try:
                     with zipfile.open(model_file_name, "r") as model_file:
-                        self._imported_model = model_file.read()
+                        self._imported_model = model_file.read().decode("utf-8")
                 except Exception as e:
                     # TODO: Change the line above to an approprate exception and handle properly
                     LOGGER.error(e)
                     exit(1)
             else:
-                LOGGER.warning(f"No models found in library {self._library}")
+                LOGGER.warning(f"No models found in library {self._library_path}")
     
 
     def __import(self) -> None:
@@ -113,20 +120,36 @@ class ImportLib:
         2. Set the footprint's model to the library name
         """
 
-        LOGGER.error(f"Function '__relink' hasn't been implemented, exiting")
-        exit(1)
+        LOGGER.warning(f"Function '__relink' is WIP")
+        
+        symbol_parser = Parser(self._imported_symbol)
+        self._imported_symbol = symbol_parser.rename_node(node_type="property", old_node_name="\"Footprint\"", old_node_name_start_only=True, new_node_name=f"\"Footprint\" \"temp\"") # TODO: Change "temp"
 
+        footprint_parser = Parser(self._imported_footprint)
+        self._imported_footprint = footprint_parser.rename_node(node_type="model", old_node_name="", old_node_name_start_only=True, new_node_name="temp.stp")
 
     def import_single(self) -> None:
         """
         """
         
-        if self._library_extension == "zip":
+        if self._input_library_extension is None:
+            self.__import()
+        elif self._input_library_extension == "zip":
             self.__import_zipped()
         else:
-            self.__import()
+            LOGGER.error(f"Unsupported library extension '.{self._input_library_extension}', exiting")
+            exit(1)
+
+        with open("symbol.txt", "w") as file:
+            file.write(self._imported_symbol)
 
         self.__relink()
+
+        with open("symbol_edited.txt", "w", newline='') as file:
+            file.write(self._imported_symbol)
+
+        with open("footprint_edited.txt", "w", newline='') as file:
+            file.write(self._imported_footprint)
 
     def import_multi(self) -> None:
         LOGGER.error(f"Function 'import_multi' hasn't been implemented, exiting")
@@ -136,9 +159,9 @@ class ImportLib:
 
 
 
-# if self._source == SOURCE_CSE:
-# elif self._source == SOURCE_EEC:
-# elif self._source == SOURCE_SNAP:
+# if self._input_source == SOURCE_CSE:
+# elif self._input_source == SOURCE_EEC:
+# elif self._input_source == SOURCE_SNAP:
 # else:
-#     LOGGER.error(f"Unsupported source '{self._source}', exiting")
+#     LOGGER.error(f"Unsupported source '{self._input_source}', exiting")
 #     exit(1)
--- source/utils.py
+++ source/utils.py
@@ -1,4 +1,7 @@
 from typing import List
+import logging
+
+LOGGER = logging.getLogger(__name__)
 
 def user_select_from_list(items: List[str], items_type: str) -> str:
     
@@ -10,16 +13,24 @@ def user_select_from_list(items: List[str], items_type: str) -> str:
     
     elif len(items) > 1:
 
+        # Print options
         print(f"\n")
         for i, item in enumerate(items):
-            print(f"{i}. {item}")
-        print(f"Choose a {items_type} from the list above")
+            print(f"[{i + 1}]\t{item}")
+        print(f"Choose a {items_type} from the list above (1 to {len(items)}) ")
 
+        # Get user input
         user_input = input().strip()
 
+        # Make sure input is an integer
         try:
-            selection = int(user_input)
+            selection = int(user_input) - 1
         except ValueError:
             LOGGER.warning(f"Invalid input '{user_input}', defaulting to first item")
+        
+        # Make sure input is in allowed range
+        if (selection < 0) or (selection >= len(items)):
+            LOGGER.warning(f"Invalid selection '{selection + 1}'. Defaulting to first item")
+            selection = 0
 
     return items[selection]
